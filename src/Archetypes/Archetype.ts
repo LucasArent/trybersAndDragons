0515d6ad041707048b52318385e99b26a4518e00
LucasArent <arentlucas2708@gmail.com>
2023-08-26 16:04:39 -0300

    feat Archetype class

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,35 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  constructor(
+    private _name: string,
+    private _cost: number = 0,
+    private _special: number = 0,
+  ) {}
+
+  //   constructor(name: string) {
+  //     this._name = name;
+  //     this._cost = 0;
+  //     this._special = 0;
+  //   }
+
+  get name() {
+    return this._name;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;

200a4b88c1b576b05a3fe153fc6e5113ccd08358
LucasArent <arentlucas2708@gmail.com>
2023-08-26 15:17:26 -0300

    create Dwarf race

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,18 @@
+import Race  from './Race';
+
+    class Dwarf extends Race {
+        private _maxLifePoints = 80;
+        private static _createdInstances = 0;
+
+        get maxLifePoints(): number {
+            return this._maxLifePoints;
+        }
+
+        static get createdInstances(): number {
+            Dwarf._createdInstances += 1;
+            return Dwarf._createdInstances;
+        }
+
+}
+
+export default Dwarf;

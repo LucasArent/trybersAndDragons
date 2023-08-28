3f98dbcf524153a83fb320ac6e4586fda58598fb
LucasArent <arentlucas2708@gmail.com>
2023-08-28 02:08:07 -0300

    Fix Figther interface errors

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,16 +1,16 @@
 import Energy from '../Energy';
 
 interface Fighter {
-    lifePoints: number;
-    strength: number;
-    defense: number;
-    energy: Energy;
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
 
-    attack(enemy: Fighter): void;
-    special(enemy: Fighter): void;
+  attack(enemy: Fighter): void;
+  special(enemy: Fighter): void;
     
-    levelUp(): void;
-    receiveDamage(attackPoints: number): number;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
 }
 
 export default Fighter;

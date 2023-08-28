4f847335d557bbbf99b90d4970b8572e3ed54bd7
LucasArent <arentlucas2708@gmail.com>
2023-08-28 01:45:23 -0300

    class Ranger from Archetype

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,22 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+class Ranger extends Archetype {
+  private _energy: EnergyType;
+  constructor(name: string) {
+    super(name);
+    this._energy = 'mana';
+  }
+
+  private static _createInstances = 0;
+  get energyType(): EnergyType {
+    return this._energy;
+  }
+
+  static createdArchetypeInstances(): number {
+    Ranger._createInstances += 1;
+    return Ranger._createInstances;
+  }
+}
+
+export default Ranger;
--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,9 +1,11 @@
 import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
+import Ranger from './Ranger';
 
 export default Archetype;
 export { 
     Mage,
     Necromancer,
+    Ranger,
 };
